0ff7f8267ab564ebd7082d5a2f1d999d92f3940f
JakubGolos <golosdev@protonmail.com>
2020-08-05 23:21:17 +0200

    Update JS

--- js/buttonscript.js
+++ js/buttonscript.js
@@ -1,3 +1,59 @@
+{
+      {
+            const buttonChangeBackground = document.querySelector(".js-button__changeBackground");
+            const body = document.querySelector(".body");
+
+            buttonChangeBackground.addEventListener("click", () => {
+                  body.classList.toggle("js-body__changeBackground");
+
+                  if (body.classList.contains("js-body__changeBackground")) {
+
+                        buttonChangeBackground.innerText = "Usun obramowanie"
+                  }
+
+                  else {
+                        buttonChangeBackground.innerText = "Dodaj obramowanie"
+                  }
+            });
+      };
+
+      {
+            const buttonChangeFont = document.querySelector(".js-button__changeFont");
+            const body = document.querySelector(".body");
+
+            buttonChangeFont.addEventListener("click", () => {
+                  body.classList.toggle("js-body__changeFont");
+
+                  if (body.classList.contains("js-body__changeFont")) {
+
+                        buttonChangeFont.innerText = "Stary Font"
+                  }
+
+                  else {
+                        buttonChangeFont.innerText = "Nowy Font"
+                  }
+            });
+      };
+
+      {
+            const buttonRemove = document.querySelector(".js-button__removeEvrything");
+            const body = document.querySelector(".body");
+
+            buttonRemove.addEventListener("click", () => {
+                  body.remove();
+            })
+      };
+
+
+}
+
+
+
+
+
+/*
+      OldCode
+
 let body = document.querySelector(".body");
 let buttonChangeBackground = document.querySelector(".js-button__changeBackground");
 let buttonChangeFont = document.querySelector(".js-button__changeFont");
@@ -16,3 +72,4 @@ buttonRemove.addEventListener("click", () => {
       body.remove();
 })
 
+*/
